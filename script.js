cbcc1d703870ccc8393bec6b2bf96e47f1aa0d74
Rodrigo Costa Santos <rodrigoscsantos@live.com>
2023-02-01 15:24:04 -0300

    script commit

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-let tela = document.querySelector('#canvas');
-let tabuleiro = tela.getContext('2d');
-tabuleiro.fillStyle = 'lightblue';
-tabuleiro.fillRect(0,0,1000,400);
+function iniciarJogo () {
+    document.querySelector("div-desaparece").style.display = "none";
+}
