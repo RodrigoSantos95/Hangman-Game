f74b3dbd895939e3564fa8495602fb6c24db1e16
Rodrigo Costa Santos <rodrigoscsantos@live.com>
2023-02-02 16:44:53 -0300

    update features

--- canvas.js
+++ canvas.js
@@ -12,3 +12,20 @@ function desenharCanvas() {
     tabuleiro.stroke();
     tabuleiro.closePath();
 }
+
+function desenharLinhas() {
+    tabuleiro.lineWidth = 6;
+    tabuleiro.lineCap = "round";
+    tabuleiro.lineJoin = "round";
+    tabuleiro.fillStyle = "#F3f5FC";
+    tabuleiro.strokeStyle = "#0A3871";
+
+    let largura = 600/palavraSecreta.length;
+    for(let i=0; i < palavraSecreta.length; i++) {
+        tabuleiro.moveTo(500+(largura), 600);
+        tabuleiro.lineTo(500+(largura), 600);
+    }
+
+    tabuleiro.stroke();
+    tabuleiro.closePath();
+}
