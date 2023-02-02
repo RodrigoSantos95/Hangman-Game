c1cec6e5f7733b18f283b0ba524ca1132ff82261
Rodrigo Costa Santos <rodrigoscsantos@live.com>
2023-02-01 23:57:30 -0300

    new features plus new js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+let palavras = ["RACE", "SOCCER", "BRASIL", "JAVASCRIPT", "REACT", "HTML"];
+let tabuleiro = document.querySelector("#forca").getContext("2d");
+let palavraSecreta = ""; 
+
+function setPalavraSecreta () {
+    let palavra = palavras[Math.floor(Math.random() * palavras.length)];
+    palavraSecreta = palavra;
+    console.log(palavraSecreta)
+}
+
 function iniciarJogo() {
     document.querySelector("#div-desaparece").style.display = "none";
+    setPalavraSecreta();
 }
