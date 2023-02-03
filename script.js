07d3af0124c1436f859839e91e81426e09e93b3b
Rodrigo Costa Santos <rodrigoscsantos@live.com>
2023-02-02 21:32:10 -0300

    update features

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 let palavras = ["RACE", "SOCCER", "BRASIL", "JAVASCRIPT", "REACT", "HTML"];
 let tabuleiro = document.querySelector("#forca").getContext("2d");
 let palavraSecreta = ""; 
+let letras = [];
 
 function setPalavraSecreta () {
     let palavra = palavras[Math.floor(Math.random() * palavras.length)];
     palavraSecreta = palavra;
+}
+
+function verificarLetra(key) {
+    let estado = false;
+    if(key >= 65 && letras.indexOf(key) || key <= 90 && letras.indexOf(key)) {
+        letras.push(key);
+        return estado;
+    }else{
+        estado = true;
+        letras.push(key);
+        return
+    }
 }
 
 function iniciarJogo() {
@@ -13,4 +26,18 @@ function iniciarJogo() {
     setPalavraSecreta();
     desenharCanvas();
     desenharLinhas();
+
+    document.onkeydown = (e) => {
+        let letra = e.key.toUpperCase();
+
+        if(verificarLetra(letra) && palavraSecreta.includes(letra)) {
+            for(let i=0; i < palavraSecreta.length; i++) {
+                if(palavraSecreta[i] === letra) {
+                    escreverLetra(i);
+                }else{
+                    mostrarLetra();
+                }
+            }
+        }
+    }
 }
